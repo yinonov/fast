fc803dda72c0f0db70d3c6ccb42f2590989b2df2
SamanthaAO <samanthaaorcutt@gmail.com>
2020-05-07 19:08:01 -0700

    add components to index.html, update component index exports to include hover-card, update main styles so that cards wrap

--- sites/fast-website/src/app/components/index.ts
+++ sites/fast-website/src/app/components/index.ts
@@ -1,3 +1,4 @@
 export { GradientPicker } from "./gradient-picker";
 export { FASTSectionHeader } from "./section-header";
 export { FastFrame } from "./fast-frame";
+export { FASTHoverCard } from "./hover-card";

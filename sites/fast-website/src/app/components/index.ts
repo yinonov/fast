0588316e309ed5d93aa715a36f2ce30690b858f0
Michael Benson <v-mbenso@microsoft.com>
2020-05-11 16:53:06 -0700

    add fast-feature-card component

--- sites/fast-website/src/app/components/index.ts
+++ sites/fast-website/src/app/components/index.ts
@@ -1,3 +1,4 @@
 export { GradientPicker } from "./gradient-picker";
 export { FASTSectionHeader } from "./section-header";
+export { FASTFeatureCard } from "./feature-card";
 export { FastFrame } from "./fast-frame";

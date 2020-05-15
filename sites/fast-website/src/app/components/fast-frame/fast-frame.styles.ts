a786ab46076b13a3b1fcea9d2a9eeb97fdeec633
John Kreitlow <john.kreitlow@microsoft.com>
2020-05-15 10:03:49 -0700

    Add svg-inline-loader

--- sites/fast-website/src/app/components/fast-frame/fast-frame.styles.ts
+++ sites/fast-website/src/app/components/fast-frame/fast-frame.styles.ts
@@ -104,12 +104,8 @@ export const FastFrameStyles = css`
         grid-column: 2;
     }
 
-    .icon-cut {
-        fill: var(--accent-foreground-cut-rest);
-    }
-
     .icon {
-        fill: var(--neutral-foreground-rest);
+        fill: currentColor;
     }
 
     .sample-control {

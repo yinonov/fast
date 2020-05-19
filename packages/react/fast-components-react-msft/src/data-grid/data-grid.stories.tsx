00def449dae29e3de4d323397d179ad213c9bdc1
Stephane Comeau <scomea@microsoft.com>
2020-05-19 14:49:20 -0700

    rough working

--- packages/react/fast-components-react-msft/src/data-grid/data-grid.stories.tsx
+++ packages/react/fast-components-react-msft/src/data-grid/data-grid.stories.tsx
@@ -1,202 +1,278 @@
-import React from "react";
 import { storiesOf } from "@storybook/react";
-import { uniqueId } from "lodash-es";
-import { TreeViewItem } from "../tree-view-item";
-import { TreeView } from "./";
+import React from "react";
+import { isEqual, omit } from "lodash-es";
+import { Omit } from "utility-types";
+import {
+    DataGrid,
+    DataGridCellProps,
+    DataGridColumnDefinition,
+    DataGridProps,
+} from "./index";
 
-const generateBeforeOrAfterContent: (className?: string) => React.ReactNode = (
-    className?: string
-): React.ReactNode => {
+function imageCellFn(
+    props: DataGridCellProps,
+    className: string,
+    cellId: React.ReactText,
+    rootElement: React.RefObject<any>,
+    focusTarget: React.RefObject<any>,
+    unhandledProps: object
+): React.ReactNode {
     return (
-        <svg className={className}>
-            <path d="M11.0625 9.79688L13 16L8 12.1562L3 16L4.9375 9.79688L0 6H6.125L8 0L9.875 6H16L11.0625 9.79688ZM11.1016 13.2812C10.9036 12.6354 10.7057 11.9948 10.5078 11.3594C10.3099 10.7188 10.1068 10.0755 9.89844 9.42969C10.4349 9.02865 10.9635 8.625 11.4844 8.21875C12.0052 7.8125 12.5312 7.40625 13.0625 7H9.14062L8 3.35156L6.85938 7H2.9375C3.46875 7.40625 3.99479 7.8125 4.51562 8.21875C5.03646 8.625 5.5651 9.02865 6.10156 9.42969C5.89323 10.0755 5.6901 10.7188 5.49219 11.3594C5.29427 11.9948 5.09635 12.6354 4.89844 13.2812L8 10.8906L11.1016 13.2812Z" />
-        </svg>
+        <div
+            {...unhandledProps}
+            data-cellid={cellId}
+            className={className}
+            ref={rootElement}
+            style={{
+                gridColumn: props.columnIndex,
+                borderWidth: "1",
+                borderColor: "black",
+                background: "white",
+                borderStyle: "solid",
+            }}
+        >
+            <img
+                src={props.rowData[props.columnDefinition.columnDataKey]}
+                alt="Placeholder image"
+                style={{
+                    width: "auto",
+                    height: "auto",
+                }}
+            />
+        </div>
     );
-};
+}
 
-const flatExample: React.ReactNode = [
-    <TreeViewItem titleContent="Favorites Bar" key={uniqueId()} />,
-    <TreeViewItem titleContent="Read later" key={uniqueId()} />,
-    <TreeViewItem titleContent="Other Favs" key={uniqueId()} />,
+function recordIdCellFn(
+    props: DataGridCellProps,
+    className: string,
+    cellId: React.ReactText,
+    rootElement: React.RefObject<any>,
+    focusTarget: React.RefObject<any>,
+    unhandledProps: object
+): React.ReactNode {
+    return (
+        <div
+            {...unhandledProps}
+            data-cellid={cellId}
+            className={className}
+            ref={rootElement}
+            style={{
+                gridColumn: props.columnIndex,
+                borderWidth: "1",
+                borderColor: "black",
+                background: "grey",
+                borderStyle: "solid",
+            }}
+        >
+            <img
+                src={`https://placehold.it/120x100/414141/?text=${
+                    props.rowData[props.columnDefinition.columnDataKey]
+                }`}
+                alt="Placeholder image"
+                style={{
+                    width: "auto",
+                    height: "auto",
+                }}
+            />
+        </div>
+    );
+}
+
+const columnDefinitions: DataGridColumnDefinition[] = [
+    {
+        columnDataKey: "recordId",
+        title: "RecordId",
+        columnWidth: "120px",
+        cell: recordIdCellFn,
+    },
+    {
+        columnDataKey: "image",
+        title: "Image",
+        columnWidth: "auto",
+        cell: imageCellFn,
+    },
 ];
 
-const nestedExample: React.ReactNode = [
-    <TreeViewItem
-        titleContent="Favorites Bar"
-        key={uniqueId()}
-        defaultExpanded={true}
-        children={[
-            <TreeViewItem titleContent="Shopping" key={uniqueId()} />,
-            <TreeViewItem
-                titleContent="Sports"
-                key={uniqueId()}
-                selected={true}
-                children={[
-                    <TreeViewItem titleContent="Football" key={uniqueId()} />,
-                    <TreeViewItem titleContent="Formula 1" key={uniqueId()} />,
-                ]}
-            />,
-        ]}
-    />,
-    <TreeViewItem titleContent="Read later" key={uniqueId()} />,
-    <TreeViewItem titleContent="Other Favs" key={uniqueId()} />,
-];
+function getDataSet(length: number): object[] {
+    const dataSet: object[] = [];
 
-const deepNestedExample: React.ReactNode = [
-    <TreeViewItem
-        titleContent="Favorites Bar"
-        key={uniqueId()}
-        beforeContent={generateBeforeOrAfterContent}
-        children={[
-            <TreeViewItem
-                titleContent="Shopping"
-                key={uniqueId()}
-                beforeContent={generateBeforeOrAfterContent}
-            />,
-            <TreeViewItem
-                titleContent="Sports"
-                key={uniqueId()}
-                beforeContent={generateBeforeOrAfterContent}
-                children={[
-                    <TreeViewItem
-                        titleContent="Football"
-                        key={uniqueId()}
-                        beforeContent={generateBeforeOrAfterContent}
-                        children={[
-                            <TreeViewItem
-                                titleContent={"Team 1"}
-                                key={uniqueId()}
-                                beforeContent={generateBeforeOrAfterContent}
-                            />,
-                            <TreeViewItem
-                                titleContent={"Team 2"}
-                                key={uniqueId()}
-                                selected={true}
-                                beforeContent={generateBeforeOrAfterContent}
-                            />,
-                            <TreeViewItem
-                                titleContent={"Team 3"}
-                                key={uniqueId()}
-                                beforeContent={generateBeforeOrAfterContent}
-                            />,
-                        ]}
-                    />,
-                    <TreeViewItem
-                        titleContent="Formula 1"
-                        beforeContent={generateBeforeOrAfterContent}
-                        key={uniqueId()}
-                    />,
-                ]}
-            />,
-        ]}
-    />,
-    <TreeViewItem
-        titleContent="Read later"
-        key={uniqueId()}
-        beforeContent={generateBeforeOrAfterContent}
-    />,
-    <TreeViewItem
-        titleContent="Other Favs"
-        key={uniqueId()}
-        beforeContent={generateBeforeOrAfterContent}
-    />,
-];
+    for (let i: number = 0; i < length; i++) {
+        const heightString: string = i % 2 ? "/150/100" : "/300/200";
+        dataSet.push({
+            recordId: `id-${i + 1}`,
+            image: `https://picsum.photos/id/${Math.floor(
+                Math.random() * 1000
+            )}${heightString}`,
+        });
+    }
 
-const nestedExampleWithBeforeContent: React.ReactNode = [
-    <TreeViewItem
-        titleContent="Favorites Bar"
-        key={uniqueId()}
-        beforeContent={generateBeforeOrAfterContent}
-        children={[
-            <TreeViewItem
-                titleContent="Shopping"
-                key={uniqueId()}
-                beforeContent={generateBeforeOrAfterContent}
-            />,
-            <TreeViewItem
-                titleContent="Sports"
-                selected={true}
-                key={uniqueId()}
-                beforeContent={generateBeforeOrAfterContent}
-                children={[
-                    <TreeViewItem
-                        titleContent="Football"
-                        key={uniqueId()}
-                        beforeContent={generateBeforeOrAfterContent}
-                    />,
-                    <TreeViewItem
-                        titleContent="Formula 1"
-                        beforeContent={generateBeforeOrAfterContent}
-                        key={uniqueId()}
-                    />,
-                ]}
-            />,
-        ]}
-    />,
-    <TreeViewItem
-        titleContent="Read later"
-        key={uniqueId()}
-        beforeContent={generateBeforeOrAfterContent}
-    />,
-    <TreeViewItem
-        titleContent="Other Favs"
-        key={uniqueId()}
-        beforeContent={generateBeforeOrAfterContent}
-    />,
-];
+    return dataSet;
+}
 
-const nestedExampleWithAfterContent: React.ReactNode = [
-    <TreeViewItem
-        titleContent="Favorites Bar"
-        key={uniqueId()}
-        defaultExpanded={true}
-        afterContent={generateBeforeOrAfterContent}
-        children={[
-            <TreeViewItem
-                titleContent="Shopping"
-                key={uniqueId()}
-                afterContent={generateBeforeOrAfterContent}
-            />,
-            <TreeViewItem
-                titleContent="Sports"
-                key={uniqueId()}
-                afterContent={generateBeforeOrAfterContent}
-                children={[
-                    <TreeViewItem
-                        titleContent="Football"
-                        key={uniqueId()}
-                        afterContent={generateBeforeOrAfterContent}
-                    />,
-                    <TreeViewItem
-                        titleContent="Formula 1"
-                        afterContent={generateBeforeOrAfterContent}
-                        key={uniqueId()}
-                    />,
-                ]}
-            />,
-        ]}
-    />,
-    <TreeViewItem
-        titleContent="Read later"
-        key={uniqueId()}
-        afterContent={generateBeforeOrAfterContent}
-    />,
-    <TreeViewItem
-        titleContent="Other Favs"
-        key={uniqueId()}
-        afterContent={generateBeforeOrAfterContent}
-    />,
-];
+function getItemHeight(
+    itemData: object,
+    rowIndex: number,
+    defaultItemHeight: number
+): number {
+    if (rowIndex % 2) {
+        return defaultItemHeight;
+    }
+    return defaultItemHeight * 2;
+}
 
-storiesOf("Tree View", module)
-    .add("Nested and default expanded", () => <TreeView children={nestedExample} />)
-    .add("Nested, beforeContent, selected child", () => (
-        <TreeView children={nestedExampleWithBeforeContent} />
+interface DataGridTestState {
+    /**
+     * The dataset to show
+     */
+    currentDataSet: object[];
+}
+
+interface DataGridTestProps extends Omit<DataGridProps, "gridData"> {
+    /**
+     * Primary data set
+     */
+    primaryDataSet: object[];
+
+    /**
+     * Secondary data set
+     */
+    secondaryDataSet: object[];
+}
+
+class DataGridTest extends React.Component<DataGridTestProps, DataGridTestState> {
+    constructor(props: DataGridTestProps) {
+        super(props);
+
+        this.state = {
+            currentDataSet: this.props.primaryDataSet,
+        };
+    }
+
+    public render(): JSX.Element {
+        const { ...props }: Partial<DataGridProps> = omit(this.props, [
+            "primaryDataSet",
+            "secondaryDataSet",
+        ]);
+
+        return (
+            <div>
+                <button
+                    style={{
+                        margin: "20px",
+                    }}
+                    onClick={this.toggleDataSetButtonClick}
+                >
+                    Toggle data
+                </button>
+                <DataGrid
+                    columnDefinitions={this.props.columnDefinitions}
+                    dataRowKey={this.props.dataRowKey}
+                    gridData={this.state.currentDataSet}
+                    itemHeight={this.props.itemHeight}
+                    {...props}
+                />
+            </div>
+        );
+    }
+
+    private toggleDataSetButtonClick = (): void => {
+        if (isEqual(this.state.currentDataSet, this.props.primaryDataSet)) {
+            this.setState({
+                currentDataSet: this.props.secondaryDataSet,
+            });
+        } else {
+            this.setState({
+                currentDataSet: this.props.primaryDataSet,
+            });
+        }
+    };
+}
+
+storiesOf("Data Grid", module)
+    .add("Detail View", () => (
+        <DataGrid
+            style={{
+                height: "300px",
+            }}
+            dataRowKey="recordId"
+            gridData={getDataSet(100)}
+            itemHeight={100}
+            columnDefinitions={columnDefinitions}
+        />
     ))
-    .add("Deeply nested, beforeContent, selected child", () => (
-        <TreeView children={deepNestedExample} />
+    .add("Initially visible row", () => (
+        <DataGrid
+            style={{
+                height: "300px",
+            }}
+            dataRowKey="recordId"
+            gridData={getDataSet(100)}
+            itemHeight={100}
+            columnDefinitions={columnDefinitions}
+            defaultFocusRowKey="id-50"
+            defaultFocusColumnKey="image"
+        />
     ))
-    .add("Nested, afterContent, default expanded", () => (
-        <TreeView children={nestedExampleWithAfterContent} />
+    .add("Empty grid", () => (
+        <DataGrid
+            style={{
+                height: "300px",
+            }}
+            dataRowKey="recordId"
+            gridData={[]}
+            itemHeight={100}
+            columnDefinitions={columnDefinitions}
+        />
     ))
-    .add("Flat example", () => <TreeView children={flatExample} />);
+    .add("Controlled Height and Width", () => (
+        <DataGrid
+            style={{
+                height: "300px",
+                width: "500px",
+            }}
+            dataRowKey="recordId"
+            gridData={getDataSet(100)}
+            itemHeight={100}
+            columnDefinitions={columnDefinitions}
+        />
+    ))
+    .add("Toggle smaller data set", () => (
+        <DataGridTest
+            style={{
+                height: "300px",
+                width: "500px",
+            }}
+            dataRowKey="recordId"
+            primaryDataSet={getDataSet(10000)}
+            secondaryDataSet={getDataSet(2000)}
+            itemHeight={100}
+            columnDefinitions={columnDefinitions}
+        />
+    ))
+    .add("Toggle empty data set", () => (
+        <DataGridTest
+            style={{
+                height: "300px",
+                width: "500px",
+            }}
+            dataRowKey="recordId"
+            primaryDataSet={getDataSet(100)}
+            secondaryDataSet={getDataSet(0)}
+            itemHeight={100}
+            columnDefinitions={columnDefinitions}
+        />
+    ))
+    .add("Variable height rows", () => (
+        <DataGrid
+            style={{
+                height: "400px",
+                width: "800px",
+            }}
+            dataRowKey="recordId"
+            gridData={getDataSet(10000)}
+            itemHeight={100}
+            columnDefinitions={columnDefinitions}
+            itemHeightCallback={getItemHeight}
+        />
+    ));
--- packages/react/fast-components-styles-msft/src/data-grid/index.ts
+++ packages/react/fast-components-styles-msft/src/data-grid/index.ts
@@ -1,33 +1,93 @@
 import { ComponentStyles } from "@microsoft/fast-jss-manager";
-import {
-    directionSwitch,
-    ellipsis,
-    format,
-    localizeSpacing,
-    toPx,
-} from "@microsoft/fast-jss-utilities";
+import { applyFocusVisible, ellipsis, format, toPx } from "@microsoft/fast-jss-utilities";
 import { DataGridClassNameContract } from "@microsoft/fast-components-class-name-contracts-msft";
 import {
+    neutralFocus,
     neutralForegroundRest,
-    neutralLayerFloating,
     neutralOutlineRest,
 } from "../utilities/color";
-import { glyphSize, height, horizontalSpacing } from "../utilities/density";
-import { applyElevation, ElevationMultiplier } from "../utilities/elevation";
-import DesignSystemDefaults, { DesignSystem } from "../design-system";
-import { applyElevatedCornerRadius } from "../utilities/border";
-import { designUnit, outlineWidth } from "../utilities/design-system";
-import { inputFieldStyles } from "../patterns/input-field";
-import { applyCursorPointer } from "../utilities/cursor";
+import { applyScaledTypeRamp } from "../utilities/typography";
+import { applyFontWeightSemiBold } from "../utilities/fonts";
+import { height, horizontalSpacing } from "../utilities/density";
+import { DesignSystem } from "../design-system";
+import { applyCornerRadius, applyFocusPlaceholderBorder } from "../utilities/border";
+import { outlineWidth } from "../utilities/design-system";
 import {
     HighContrastColor,
-    highContrastDisabledForeground,
     highContrastForeground,
     highContrastOptOutProperty,
-    highContrastOutline,
     highContrastSelector,
 } from "../utilities/high-contrast";
 
-const styles: ComponentStyles<DataGridClassNameContract, DesignSystem> = {};
+const styles: ComponentStyles<DataGridClassNameContract, DesignSystem> = {
+    dataGrid: {
+        display: "flex",
+        "flex-direction": "column",
+        "justify-content": "flex-start",
+        [highContrastSelector]: {
+            ...highContrastOptOutProperty,
+        },
+        "& $dataGrid_header, $dataGrid_cell": {
+            ...highContrastForeground,
+        },
+    },
+    dataGrid_header: {
+        color: neutralForegroundRest,
+        fill: neutralForegroundRest,
+        height: height(),
+        ...applyScaledTypeRamp("t7"),
+        ...applyFontWeightSemiBold(),
+        "border-bottom": format<DesignSystem>(
+            "{0} solid {1}",
+            toPx(outlineWidth),
+            neutralOutlineRest
+        ),
+    },
+    dataGrid_columnHeader: {
+        height: "100%",
+        "box-sizing": "border-box",
+        overflow: "hidden",
+        padding: format<DesignSystem>("0 {0}", horizontalSpacing(0)),
+        ...ellipsis(),
+        "line-height": "26px",
+    },
+    dataGrid_viewport: {
+        height: "100%",
+        width: "100%",
+        "overflow-y": "auto",
+        "overflow-x": "hidden",
+    },
+    dataGrid_gridContainer: {},
+    dataGrid__isScrolling: {},
+    dataGrid_row: {
+        padding: "1px 0",
+        "box-sizing": "border-box",
+        width: "100%",
+        "border-bottom": format<DesignSystem>(
+            "{0} solid {1}",
+            toPx(outlineWidth),
+            neutralOutlineRest
+        ),
+    },
+    dataGrid_row__focusWithin: {},
+    dataGrid_cell: {
+        color: neutralForegroundRest,
+        fill: neutralForegroundRest,
+        "box-sizing": "border-box",
+        overflow: "hidden",
+        padding: format<DesignSystem>("0 {0}", horizontalSpacing(0)),
+        ...ellipsis(),
+        ...applyScaledTypeRamp("t7"),
+        ...applyCornerRadius(),
+        ...applyFocusPlaceholderBorder(),
+        ...applyFocusVisible<DesignSystem>({
+            "border-color": neutralFocus,
+            [highContrastSelector]: {
+                "border-color": HighContrastColor.selectedBackground,
+            },
+        }),
+        "line-height": "26px",
+    },
+};
 
 export default styles;

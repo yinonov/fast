517aa18e8b38decfc270784c88ec12730733825e
Stephane Comeau <scomea@microsoft.com>
2020-05-28 17:30:31 -0700

    non-virtualized mode like it should pt1

--- packages/react/fast-components-react-base/src/data-grid/data-grid.stories.tsx
+++ packages/react/fast-components-react-base/src/data-grid/data-grid.stories.tsx
@@ -335,4 +335,17 @@ storiesOf("Data Grid", module)
             rowHeightCallback={getRowHeight}
             virtualize={true}
         />
+    ))
+    .add("No virtualization", () => (
+        <DataGrid
+            style={{
+                height: "300px",
+            }}
+            virtualizeItems={false}
+            dataRowKey="recordId"
+            gridData={getDataSet(100)}
+            defaultFocusRowKey="id-100"
+            rowHeight={100}
+            columnDefinitions={columnDefinitions}
+        />
     ));

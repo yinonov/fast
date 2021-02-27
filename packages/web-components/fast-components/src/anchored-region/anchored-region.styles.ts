4789ec5a34d619b3b192a17d1736ad9936011938
Stephane Comeau <scomea@microsoft.com>
2021-02-27 12:41:55 -0800

    working

--- packages/web-components/fast-components/src/anchored-region/anchored-region.styles.ts
+++ packages/web-components/fast-components/src/anchored-region/anchored-region.styles.ts
@@ -4,12 +4,5 @@ export const AnchoredRegionStyles = css`
     :host {
         contain: layout;
         display: block;
-        // pointer-events: none;
-        // opacity: 0;
     }
-
-    // :host(.loaded) {
-    //     pointer-events: auto;
-    //     opacity: 1;
-    // }
 `;
